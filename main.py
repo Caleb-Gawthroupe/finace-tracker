ebdbca10524d19963367f55412a7f36a8e669e91
Caleb-Gawthroupe <116599077+Caleb-Gawthroupe@users.noreply.github.com>
2024-06-03 21:35:02 -0400

    Update main.py
    Saving Goals Addition.

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
     entry_type_error = "Error: Invalid Entry Type"
     out_of_range_error = "Error: Out of Range"
     account = BankAccount.import_account()
+    print(account.goals)
+    print(account.checkings)
+    BankAccount.load_saving_goals(account)
     
     while True:
         #Round the account numbers
@@ -22,7 +25,7 @@ def main():
             action = 0
             try:
                 print("What would you like to do?")
-                action = int(input("Reset Account (1) \nAdd Paycheck (2) \nSpend Money (3) \nTransfer Money to Savings (4) \nView Balance (5) \nExit Program(6)\n"))
+                action = int(input("Reset Account (1) \nAdd Paycheck (2) \nSpend Money (3) \nSavings Account (4) \nView Balance (5) \nExit Program(6)\n"))
             except:
                 print(entry_type_error)
             if action < 1 or action > 6:
@@ -36,7 +39,24 @@ def main():
         if action == 3:
             account = BankAccount.spend_money(account)
         if action == 4:
-            account = BankAccount.transfer_savings(account)
+            # All actions for savings account
+            while True:
+                saving_action = 0
+                try:
+                    print("What would you like to do?")
+                    saving_action = int(input("View Goals (1) \nCreate New Goal (2) \nTransfer Money to Savings (3) \n"))
+                except:
+                    print(entry_type_error)
+                if saving_action < 1 or saving_action > 3:
+                    print(out_of_range_error)
+                else:
+                    break
+            if saving_action == 1:
+                account.view_saving_goals()
+            if saving_action == 2:
+                account.create_saving_goal()
+            if saving_action == 3:
+                account.transfer_savings(account)
         if action == 5:
             BankAccount.view_balances(account)
         if action == 6:
